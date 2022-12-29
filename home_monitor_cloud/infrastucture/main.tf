843c583b343db3291d221117c5f18c45e13576f1
Jack Thomson <jackthomson@jacks-mbp.lan>
2022-12-29 15:37:06 +0000

    restrict access to specific IP

--- home_monitor_cloud/infrastucture/main.tf
+++ home_monitor_cloud/infrastucture/main.tf
@@ -246,35 +246,35 @@ resource "google_compute_firewall" "ssh_rule" {
   project     = var.project
   name        = "allow-ssh"
   network     = google_compute_network.vpc_network.name
-  description = "Allow SSH inbound traffic from all IP ranges"
+  description = "Allow SSH inbound traffic from specific IP"
 
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
 
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = [data.sops_file.secrets.data["ip_address"]]
 }
 
 resource "google_compute_firewall" "emqx_dashboard_rule" {
   project     = var.project
   name        = "emqx-dashboard"
   network     = google_compute_network.vpc_network.name
-  description = "Allow ingress traffic to emqx dashboard"
+  description = "Allow ingress traffic to emqx dashboard from specific IP"
 
   allow {
     protocol = "tcp"
     ports    = ["18083"]
   }
 
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = [data.sops_file.secrets.data["ip_address"]]
 }
 
 resource "google_compute_firewall" "emqx_tcp" {
   project     = var.project
   name        = "emqx-tcp"
   network     = google_compute_network.vpc_network.name
-  description = "Allow ingress traffic to emqx tcp"
+  description = "Allow ingress traffic to emqx tcp (IOT Devices)"
 
   allow {
     protocol = "tcp"
