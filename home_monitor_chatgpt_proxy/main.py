d41969b8f8129e506390f84c2517775d1a37955a
Jack Thomson <jackthomson@jacks-mbp.lan>
2023-07-22 22:48:56 +0100

    add proxy ready for chatGPT plugin

---
+++ home_monitor_chatgpt_proxy/main.py
@@ -0,0 +1,34 @@
+import quart
+import quart_cors
+from quart import request
+
+app = quart_cors.cors(quart.Quart(__name__),
+                      allow_origin="https://chat.openai.com")
+
+
+@app.get("/logo.png")
+async def plugin_logo():
+    filename = 'logo.png'
+    return await quart.send_file(filename, mimetype='image/png')
+
+
+@app.get("/.well-known/ai-plugin.json")
+async def plugin_manifest():
+    with open("./.well-known/ai-plugin.json") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/json")
+
+
+@app.get("/openapi.yaml")
+async def openapi_spec():
+    with open("openapi.yaml") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/yaml")
+
+
+def main():
+    app.run(debug=True, host="0.0.0.0", port=5003)
+
+
+if __name__ == "__main__":
+    main()
