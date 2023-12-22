834e180ae50f0f22ee4c07fc24f38823588035ca
Jack Thomson <hello@jackthomson.co.uk>
2023-12-22 19:46:37 +0000

    add caching layer for energy consumption query

--- home_monitor_cloud/functions/getEnergyConsumption.go
+++ home_monitor_cloud/functions/getEnergyConsumption.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
+	"time"
 
 	"jackthomson.com/functions/models"
 	"jackthomson.com/functions/services"
@@ -22,11 +23,26 @@ func GetEnergyConsumption(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Access-Control-Allow-Origin", originToUse)
 
+	var res []models.EnergyConsumption = make([]models.EnergyConsumption, 0)
+
+	if err := services.GetDataFromRedis(&res, "energy_consumption"); err == nil {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+
 	query := `SELECT REGEXP_EXTRACT(payload, r'alias: ([^,}]+)') AS alias, CEIL(AVG(CAST(REGEXP_EXTRACT(payload, r'power_mw: (\d+)') as INT64) )) as power_wh_avg FROM ` + "`home-monitor-373013.home_monitor_dataset.home_monitor_table`" + ` WHERE timestamp >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 1 HOUR) GROUP BY alias`
 
 	response, err := services.GetDataFromBigQuery[models.EnergyConsumption](context.Background(), "home_monitor_table", query)
 
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	delta := time.Duration(1) * time.Hour
+	if err := services.CreateDataInRedis(response, "energy_consumption", delta); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(err)
 		return
